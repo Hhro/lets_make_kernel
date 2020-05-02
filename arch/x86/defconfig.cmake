850b994dd08822ab6527e34a2822369273f8989a
ohk990102 <ohk990102@korea.ac.kr>
2020-05-02 01:25:36 -0700

    [Add] cmake build system, check README

---
+++ arch/x86/defconfig.cmake
@@ -0,0 +1,14 @@
+set(LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/boot/linker.ld)
+include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ffreestanding -fno-exceptions -fno-rtti -fPIC -fPIE -no-pie")
+
+if(CMAKE_BUILD_TYPE EQUAL "RELEASE")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2")
+elseif(CMAKE_BUILD_TYPE EQUAL "DEBUG")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra")
+endif()
+
+set(LINKER_FLAGS "-z max-page-size=0x1000 -nostdlib -lgcc -T ${LINKER_SCRIPT}")
+
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${LINKER_FLAGS}")
+set(CMAKE_ASM_FLAGS "${CMAKE_CXX_FLAGS} -x assembler-with-cpp -Wa,-I -Wa,include")
