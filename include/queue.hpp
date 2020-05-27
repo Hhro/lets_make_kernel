53f3d1d1fa0b9f39719b0d2e932f2e8df26804c0
oreoqupang <cws1104@korea.ac.kr>
2020-05-27 12:51:16 +0900

    [Fixed] build success but driver not working

--- include/queue.hpp
+++ include/queue.hpp
@@ -22,4 +22,40 @@ public:
 	bool isFull();
 };
 
+template<class T>
+bool Queue<T>::push(T value){
+    if(!isFull()){
+	    values[rear] = value;
+		rear = (rear + 1) % BUFFERSIZE;
+        return true;
+	}
+	return false;
+}
+
+template<class T>
+bool Queue<T>::pop(T* target){
+    if(!isEmpty()){
+        *target = values[front];
+        front = (front + 1) % BUFFERSIZE;
+        return true;
+    }
+    return false;
+}
+
+template<class T>
+bool Queue<T>::isEmpty(){
+    if(rear == front)
+			return true;
+		else 
+			return false;
+}
+
+template<class T>
+bool Queue<T>::isFull(){
+    if((rear + 1) % BUFFERSIZE == front) 
+		return true;
+	else 
+		return false;
+}
+
 #endif
