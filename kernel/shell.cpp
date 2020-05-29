6dba7c3951e80cffb29c10b1caa13c547f94b296
ohk990102 <ohk990102@gmail.com>
2020-05-29 14:12:39 +0900

    [Add] shell implementation

---
+++ kernel/shell.cpp
@@ -0,0 +1,83 @@
+#include <fried/stdio.h>
+#include <shell.hpp>
+#include <keyboard.hpp>
+#include <fried/string.h>
+
+#define BUILTIN_CMDS_COUNT  (sizeof(builtin_cmds) / sizeof(struct BUILTIN_CMDS))
+
+struct BUILTIN_CMDS {
+    void (*function)(char *);
+    const char *name;
+    const char *desc;
+};
+
+void help(char *args);
+void echo(char *args);
+
+BUILTIN_CMDS builtin_cmds[] = {
+    {.function = help, .name = "help", .desc = "Print shell help document. "},
+    {.function = echo, .name = "echo", .desc = "Echo args to output. "},
+};
+
+void help(char *args) {
+    kPrintf("List of cmds: \n");
+    for(int i = 0; i < BUILTIN_CMDS_COUNT; i++) {
+        kPrintf("%s: %s\n", builtin_cmds[i].name, builtin_cmds[i].desc);
+    }
+}
+
+void echo(char *args) {
+    kPrintf("%s\n", args);
+}
+
+void execute_command(char *command_buf) {
+    size_t command_buf_len = strlen(command_buf);
+    size_t command_len = command_buf_len;
+    for (int i = 0; i < command_buf_len; i++) {
+        if (command_buf[i] == ' ') {
+            command_len = i;
+            break;
+        }
+    }
+
+    for(int i = 0; i < BUILTIN_CMDS_COUNT; i++) {
+        size_t builtin_command_name_len = strlen(builtin_cmds[i].name);
+        if (command_len == builtin_command_name_len && memcmp(command_buf, builtin_cmds[i].name, command_len) == 0) {
+            builtin_cmds[i].function(command_buf + command_len + 1);
+        } 
+    }
+}
+
+void shell_main() {
+    kPrintf("> ");
+
+    char command_buf[300];
+    int command_buf_idx = 0;
+    
+    while(1) {
+        char key = kGetch();
+
+        if (key == KEY_BACKSPACE) {
+            // TODO: backspace support
+        }
+        else if (key == KEY_ENTER) {
+            kPrintf("\n");
+            if (command_buf_idx > 0) {
+                command_buf[command_buf_idx] = '\x00';
+                execute_command(command_buf);
+            }
+            kPrintf("> ");
+            memset(command_buf, 0, sizeof(command_buf));
+            command_buf_idx = 0;
+        }
+        else {
+            if (key == KEY_TAB) {
+                key = ' ';
+            }
+            if (command_buf_idx < sizeof(command_buf)) {
+                command_buf[command_buf_idx++] = key;
+                kPrintf("%c", key);
+            }
+        }
+    }
+}
