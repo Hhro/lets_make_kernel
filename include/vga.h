e3caba08afa3b883bd3f1f63080b34e8d3f2d785
ohk990102 <ohk990102@gmail.com>
2020-05-29 12:02:37 +0900

    [Add terminal newline support

--- include/vga.h
+++ include/vga.h
@@ -64,13 +64,23 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
     const size_t index = y * VGA_WIDTH + x;
     terminal_buffer[index] = vga_entry(c, color);
 }
-
-extern "C" void terminal_putchar(char c) {
-    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-    if (++terminal_column == VGA_WIDTH) {
-        terminal_column = 0;
-        if (++terminal_row == VGA_HEIGHT) terminal_row = 0;
-    }
+ 
+extern "C" void terminal_putchar(char c) 
+{
+	unsigned char uc = c;
+	if(uc == '\n') {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT)
+			terminal_row = 0;
+	}
+	else {
+		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+		if (++terminal_column == VGA_WIDTH) {
+			terminal_column = 0;
+			if (++terminal_row == VGA_HEIGHT)
+				terminal_row = 0;
+		}
+	}
 }
 
 void terminal_write(const char* data, size_t size) {
