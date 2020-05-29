20ae1935b6f1376dd54406245572a4e0b1aadf7e
ohk990102 <ohk990102@gmail.com>
2020-05-29 14:19:41 +0900

    [Fix] mark as weak attr to memcpy

--- lib/memcpy.cpp
+++ lib/memcpy.cpp
@@ -1,6 +1,6 @@
 #include <fried/string.h>
 
-void *memcpy(void *dstptr, const void *srcptr, uint64_t size) {
+void __attribute__((weak)) *memcpy(void *dstptr, const void *srcptr, unsigned long long size) {
 	unsigned char* dst = (unsigned char*) dstptr;
 	const unsigned char* src = (unsigned char*) srcptr;
 	for (size_t i = 0; i < size; i++)
